ed08bcf95e2d1dd13426e92b7bb97420019593c8
ggorilla11-hub <ggorilla11@gmail.com>
2026-03-13 05:18:12 +0900

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   credentials: false
 }));
-app.options('(.*)', cors());
 app.use(express.json());
 app.use(express.static('public'));
 
